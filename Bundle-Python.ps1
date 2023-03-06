9f072c8ee4e29e02020e0d6b8c7629fc446c3b77
Juraj Michalek <juraj.michalek@espressif.com>
2023-03-06 16:14:56 +0100

    tools: simplify Python build

--- Bundle-Python.ps1
+++ Bundle-Python.ps1
@@ -33,19 +33,11 @@ if ($True -eq $InstallVirtualenv) {
 # Venv helper files must be extracted from main python.
 # Embedded version does not contain working copy for virtualenv.
 # If Python is not available then install one
-if ($null -eq (Get-Command "python.exe" -ErrorAction SilentlyContinue))  {
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}-amd64.exe" -Out "python-amd64.exe"
-    .\python-amd64.exe /quiet /passive TargetDir=${pwd}\temp-python3
-    $InstallerProcess = Get-Process python-amd64
-    Wait-Process -Id $InstallerProcess.id
-    $PythonVenvScripts = temp-python3\Lib\venv\scripts\nt
-} else {
-    if ($True -eq $InstallVirtualenv) {
-        python -m pip install virtualenv
-        python -m virtualenv temp-python3
-    }
-    $PythonVenvScripts = "temp-python3\Scripts"
-}
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}-amd64.exe" -Out "python-amd64.exe"
+.\python-amd64.exe /quiet /passive TargetDir=${pwd}\temp-python3
+$InstallerProcess = Get-Process python-amd64
+Wait-Process -Id $InstallerProcess.id
+$PythonVenvScripts = temp-python3\Lib\venv\scripts\nt
 
 mkdir ${PythonDirectory}\Lib\venv\scripts\nt
 Copy-Item ${PythonVenvScripts}\python.exe ${PythonDirectory}\Lib\venv\scripts\nt
