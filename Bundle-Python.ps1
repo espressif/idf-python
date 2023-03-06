96426eb1302fff22bde58ff43518c3e98d7d255c
Juraj Michalek <juraj.michalek@espressif.com>
2023-03-06 16:09:02 +0100

    tools: venv still requires binary from outside

--- Bundle-Python.ps1
+++ Bundle-Python.ps1
@@ -28,25 +28,28 @@ Invoke-WebRequest -Uri "https://bootstrap.pypa.io/get-pip.py" -Out "get-pip.py"
 
 if ($True -eq $InstallVirtualenv) {
     & .\${PythonDirectory}\python.exe -m pip install virtualenv
+}
 
-    # Virtualenv helper files must be extracted from main python.
-    # Embedded version does not contain working copy for virtualenv.
-    # If Python is not available then install one
-    if ($null -eq (Get-Command "python.exe" -ErrorAction SilentlyContinue))  {
-        Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}-amd64.exe" -Out "python-amd64.exe"
-        .\python-amd64.exe /quiet /passive TargetDir=${pwd}\temp-python3
-        $InstallerProcess = Get-Process python-amd64
-        Wait-Process -Id $InstallerProcess.id
-        $PythonVenvScripts = temp-python3\Lib\venv\scripts\nt
-    } else {
+# Venv helper files must be extracted from main python.
+# Embedded version does not contain working copy for virtualenv.
+# If Python is not available then install one
+if ($null -eq (Get-Command "python.exe" -ErrorAction SilentlyContinue))  {
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}-amd64.exe" -Out "python-amd64.exe"
+    .\python-amd64.exe /quiet /passive TargetDir=${pwd}\temp-python3
+    $InstallerProcess = Get-Process python-amd64
+    Wait-Process -Id $InstallerProcess.id
+    $PythonVenvScripts = temp-python3\Lib\venv\scripts\nt
+} else {
+    if ($True -eq $InstallVirtualenv) {
         python -m pip install virtualenv
         python -m virtualenv temp-python3
-        $PythonVenvScripts = "temp-python3\Scripts"
     }
-    mkdir ${PythonDirectory}\Lib\venv\scripts\nt
-    Copy-Item ${PythonVenvScripts}\python.exe ${PythonDirectory}\Lib\venv\scripts\nt
-    Copy-Item ${PythonVenvScripts}\pythonw.exe ${PythonDirectory}\Lib\venv\scripts\nt
+    $PythonVenvScripts = "temp-python3\Scripts"
 }
+
+mkdir ${PythonDirectory}\Lib\venv\scripts\nt
+Copy-Item ${PythonVenvScripts}\python.exe ${PythonDirectory}\Lib\venv\scripts\nt
+Copy-Item ${PythonVenvScripts}\pythonw.exe ${PythonDirectory}\Lib\venv\scripts\nt
 
 # Create final zip - GitHub performs compression of artifacts automatically
 Compress-Archive -Path "python\*" -DestinationPath "python.zip"
